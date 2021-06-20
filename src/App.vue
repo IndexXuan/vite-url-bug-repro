3ece1e38050d5170d9e977a06291189454b55e6f
IndexXuan <indexxuan@gmail.com>
2021-06-20 19:14:25 +0800

    feat: first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <!-- ~@/assets/logo.png -->
+  <img alt="Vue logo" src="~@/assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <div class="hello">hello world</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.hello {
+  background: url(~@/assets/logo.png) 0 0 no-repeat;
+}
+</style>
